135b149911fa28b9b5e14be786e26791ad2c1682
Christer van der Meeren <cmeeren@gmail.com>
2015-06-01 00:14:27 +0200

    remove unsupported theme option from docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,3 @@ html_sidebars = {
    '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
 }
 html_short_title = '%s-%s' % (project, version)
-html_theme_options = {
-    'githuburl': 'https://github.com/cmeeren/latexdiffcite/'
-}
