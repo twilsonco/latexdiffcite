90296505ab4d2498766d704618b703912d7bdd10
Christer van der Meeren <cmeeren@gmail.com>
2015-06-01 22:19:57 +0200

    change classifiers in setup.py

--- setup.py
+++ setup.py
@@ -39,19 +39,18 @@ setup(
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
         'Operating System :: Unix',
-        'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Text Processing :: Markup :: LaTeX',
         'Topic :: Utilities',
     ],
     keywords=[
