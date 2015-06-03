867db7bd7e211b074b53a3ae1d524809bb6ea5a7
Christer van der Meeren <cmeeren@gmail.com>
2015-06-04 00:44:36 +0200

    add Read The Docs theme for local compilation

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,6 @@ from __future__ import unicode_literals
 
 import os
 
-import sphinx_rtd_theme
-
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
@@ -34,6 +31,14 @@ html_use_smartypants = True
 html_last_updated_fmt = '%b %d, %Y'
 html_split_index = True
 html_sidebars = {
-   '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
+    '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
 }
 html_short_title = '%s-%s' % (project, version)
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
