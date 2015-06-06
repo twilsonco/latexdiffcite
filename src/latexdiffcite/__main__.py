671e442b81a4994e56310785a51a136aa8b45f0e
Christer van der Meeren <cmeeren@gmail.com>
2015-06-06 21:02:50 +0200

    slightly change description of __main__

--- src/latexdiffcite/__main__.py
+++ src/latexdiffcite/__main__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
-'''latexdiffcite.__main__: executed when latexdiffcite directory is called as script.'''
+'''latexdiffcite.__main__: executed when latexdiffcite is invoked with python -m latexdiffcite'''
 
 
 import sys
 
 from .latexdiffcite import main
+
 
 if __name__ == '__main__':
     sys.exit(main())
